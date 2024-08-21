b5aa181823681a41b34e603b3947d3bca691e090
Kailan de Scena Pacheco <kailanscea@gmail.com>
2024-08-20 21:17:53 -0300

    Adição dos Arquivos

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int numeroAleatorio = random.Next(1, 101); // Gera um número aleatório entre 1 e 100
+int palpite = 0;
+int tentativas = 0;
+
+Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
+Console.WriteLine("Tente adivinhar o número que estou pensando, entre 1 e 100.");
+
+while (palpite != numeroAleatorio)
+{
+    Console.WriteLine("Digite seu palpite:");
+    palpite = Convert.ToInt32(Console.ReadLine());
+    tentativas++;
+
+    if (palpite < numeroAleatorio)
+    {
+        Console.WriteLine("Muito baixo! Tente novamente.");
+    }
+    else if (palpite > numeroAleatorio)
+    {
+        Console.WriteLine("Muito alto! Tente novamente.");
+    }
+    else
+    {
+        Console.WriteLine("Parabéns! Você adivinhou o número em " + tentativas + " tentativas.");
+    }
+}
+
+
+Console.WriteLine("Obrigado por jogar o Jogo de Adivinhação!");
